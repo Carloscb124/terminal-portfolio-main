ec3b8185b7acfe0026c60f89fc183aea5badea9d
Carlos Eduardo <cb473355@gmail.com>
2025-06-01 16:49:50 -0300

    Minhas alterações

--- src/js/About.js
+++ src/js/About.js
@@ -4,7 +4,8 @@ export class About {
     toString() {
         return `<p>Sou o Carlos, estudante do <b>Senac</b>, <b>atualmente cursando TI, mas já fiz Análise e desenvolvimento de sistemas.</b> Sou natural de São Paulo. Sempre gostei de tecnologia e hoje busco aprimorar minhas habilidades todos os dias. Estou mais envolvido com desenvolvimento web, utilizando JavaScript, CSS e HTML. Também estou estudando para me tornar um profissional de Design UI/UX. Quando não estou codando ou estudando, estou jogando xadrez, escutando música ou tocando guitarra. Gosto de ler e de explorar um mundo introspectivo.
         Aqui vai um pouco do que gosto de escutar:
-        <iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/1WuqlDJb7Z2ipBhM6ww7QI?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        <iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/1WuqlDJb7Z2ipBhM6ww7QI?utm_source=generator&theme=0" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy">
+        </iframe>
         </p>`;
     }
     
